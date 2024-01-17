619b074f027a0e6504c75025b30a392924621eb1
Gagan Malvi <malvigagan@gmail.com>
2024-01-17 21:09:51 +0000

    X6837: Import `mtk_plpath_utils`
    Author: Gagan Malvi <malvigagan@gmail.com>
    Date: Mon, 10 Jan 2022 06:15:40 +0100
    
        rosemary: Compile and install mtk_plpath_utils.
    
    Author: Woomy <woomy@woomy.be>
    Date: Sun, 6 Mar 2022 15:32:46 +0100
    
        MTK-PlPath-Utils: Fix compilation errors
    
    Author: Woomy <woomy@woomy.be>
    Date: Thu, 23 Jun 2022 18:45:31 +0200
    
        mtk_plpath_utils: Cleanup code
    
        * Use ALOGD() for debug informations
        * Remove EMMC Pl support (not needed on rosemary)
        * Format code
    
    Change-Id: I3f44bbd97ee0777206b0b22f0d26ff2707961f6f
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>
    Co-Authored-By: Woomy <woomy@woomy.be>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ AB_OTA_PARTITIONS := \
     vendor_boot \
     vendor_dlkm
 
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
