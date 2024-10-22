aa6ddc38e828a1c27b9030b092fa1caf704e5459
fjrXTR <fajarslebew31@gmail.com>
2024-10-22 10:47:07 +0000

    X6837: Evolution? yes
    Change-Id: I9aa2ae7cac9569650636212144442a452eb45204
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- lineage_X6837.mk
+++ lineage_X6837.mk
@@ -8,17 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common EvolutionX stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_MINI_GAPPS := true
-PRODUCT_NO_CAMERA := false
+TARGET_BUILD_APERTURE_CAMERA := true
+-include vendor/lineage-priv/keys/keys.mk
 
 # Inherit from X6837 device
 $(call inherit-product, device/infinix/X6837/device.mk)
 
+# Device identifier. This must come after all inclusions.
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := derp_X6837
+PRODUCT_NAME := lineage_X6837
 PRODUCT_DEVICE := X6837
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
