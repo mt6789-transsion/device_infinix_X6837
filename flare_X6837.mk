5dae6ddf42c91c840bde46bb1a83988ecb2379a4
fjrXTR <fajarslebew31@gmail.com>
2025-02-17 14:30:37 +0000

    X6837: Hi Flare!
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- flare_X6837.mk
+++ flare_X6837.mk
@@ -8,14 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Flare stuff.
+$(call inherit-product, vendor/flare/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+FLARE_BUILD_TYPE := UNOFFICIAL
+FLARE_MAINTAINER := gabutuniverse!
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
 
 # Inherit from X6837 device
 $(call inherit-product, device/infinix/X6837/device.mk)
 
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6837
+PRODUCT_NAME := flare_X6837
 PRODUCT_DEVICE := X6837
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -26,6 +32,3 @@ PRODUCT_SYSTEM_NAME := X6837-OP
 PRODUCT_SYSTEM_DEVICE := X6837
     
 BUILD_FINGERPRINT := Infinix/X6837-OP/Infinix-X6837:13/TP1A.220624.014/240929V1624:user/release-keys
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
