fdc245fc6c2172569a064eaa14584025695e422d
Edwin Moquete <edwinmmoquete@gmail.com>
2025-05-14 00:13:26 +0800

    X6837: Move DT2W handling to powerhint
    Shirayuki39: Also drop powerhint extension
    
    Change-Id: Icd035962d289996feb76ddeaf02bf1843bd9d64a
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,10 +115,6 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/gesture_function"
-TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-transsion
-
 # Properties
 TARGET_SYSTEM_PROP += $(CONFIGS_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(CONFIGS_PATH)/properties/vendor.prop
