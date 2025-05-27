4fd8b9f6f827fcd8b48b067ed6cde7a20aac8479
bengris32 <bengris32@protonmail.ch>
2025-05-27 22:58:27 +0800

    X6837: Move to OSS libfmjni
    Change-Id: I5dd6efb85c379afa4557ed3d98b42d90c18bf806
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,7 @@ TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
 # Properties
+TARGET_PRODUCT_PROP += $(CONFIGS_PATH)/properties/product.prop
 TARGET_SYSTEM_PROP += $(CONFIGS_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(CONFIGS_PATH)/properties/vendor.prop
 
