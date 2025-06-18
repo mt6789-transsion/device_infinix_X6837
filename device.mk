f6167c0f1d4b27f2a693d0fb6054c93ef672f4c5
fjrXTR <fajarslebew31@gmail.com>
2025-06-18 11:22:52 +0000

    X6837: Switch to AOSP Bluetooth audio
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -100,7 +100,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor
+    android.hardware.bluetooth.audio-impl:64
+
+PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.bluetooth.audio@2.1.vendor \
+    vendor.mediatek.hardware.bluetooth.audio@2.2.vendor
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
