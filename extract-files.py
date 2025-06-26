40c56cc4a257b6ee4341faf59b0702ee4010f616
ShevT <evgeny.shishkov@gmail.com>
2025-06-26 09:45:04 +0900

    X6837: Switch to AIDL NXP NFC HAL
    Change-Id: Iff03bd7fc522b45b9caebb12860c5c851356a9a1
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -77,8 +77,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
         .regex_replace('1.1', '1.2'),
 
-    ('vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so', 'vendor/lib64/libstfactory-vendor.so', 'vendor/lib64/ese_spi_nxp.so', 
-     'vendor/lib64/nfc_nci_nxp.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so', 'vendor/lib64/libtflite_mtk.so'): blob_fixup()
+    ('vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so', 'vendor/lib64/libnvram.so', 
+      'vendor/lib64/libsysenv.so', 'vendor/lib64/libtflite_mtk.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
 
     'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
