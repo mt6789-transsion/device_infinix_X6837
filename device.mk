6c8af311cb7cee92087505949f97b31fc4d835ac
fjrXTR <fajarslebew31@gmail.com>
2025-08-26 12:08:26 +0200

    X6837: Stop build DRM vendor
    Change-Id: I62b25f4aba6ae35da99a4ae040b92a903c3f13dd
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -157,9 +157,6 @@ $(call inherit-product, vendor/sony/dolby/setup.mk)
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4.vendor
-
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
