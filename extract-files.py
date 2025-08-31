799568d6a12bead7aec16f8cf9c747ff34859893
fjrXTR <fajarslebew31@gmail.com>
2025-08-31 15:45:56 +0200

    X6837: 22.2
    Change-Id: I2bd5340bffea95a85b4993926bb299d616086190
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -74,10 +74,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
         .patchelf_version('0_17_2')
         .add_needed('libprocessgroup_shim.so'),
-    ('vendor/lib64/mt6789/lib3a.flash.so', 'vendor/lib64/mt6789/lib3a.ae.stat.so', 'vendor/lib64/mt6789/lib3a.sensors.flicker.so', 'vendor/lib64/mt6789/lib3a.sensors.color.so', 'vendor/lib64/mt6789/libaaa_ltm.so', 'vendor/lib64/libSQLiteModule_VER_ALL.so'): blob_fixup()
-        .add_needed('liblog.so'),
-    'vendor/lib64/mt6789/libmnl.so': blob_fixup()
-        .add_needed('libcutils.so'),
     'vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_createFromHandle')
@@ -95,10 +91,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
-    'vendor/bin/hw/mtkfusionrild': blob_fixup()
-        .add_needed('libutils-v32.so'),
-    'vendor/lib64/mt6789/libmorpho_video_stabilizer.so': blob_fixup()
-        .add_needed('libutils-v32.so'),
     ('vendor/lib64/hw/mt6789/vendor.mediatek.hardware.camera.isphal@1.0-impl.so', 'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.camera.isphal@1.1-impl.so'): blob_fixup()
         .replace_needed('libhidlbase.so', 'libhidlbase-v31.so')
         .replace_needed('libbinder.so', 'libbinder-v31.so')
